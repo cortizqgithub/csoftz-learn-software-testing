6a25c8194612877f610998cd50e25b9125cb2715
Carlos Adolfo Ortiz Q (Ceiba) <carlosaortiz@yahoo.com>
2017-09-18 14:06:16 -0500

    Creates palindrome library.

---
+++ palindrome/language/csharp/framework/palindrome-framework/palindrome-lib-tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("palindrome-lib-tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("palindrome-lib-tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("30244da5-72e0-4a99-9441-ef31c82bb8fa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
